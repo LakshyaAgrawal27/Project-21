0d4bf9fbdc6a797b23857dd54788fde2a7b7900b
LakshyaAgrawal27 <76614024+LakshyaAgrawal27@users.noreply.github.com>
2021-01-16 18:38:29 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var canvas;
-var music;
+var music, edges;
 var o1, o2, o3, o4, ball;
 
 function preload(){
@@ -8,17 +8,7 @@ function preload(){
 
 
 function setup(){
-    canvas = createCanvas(800,600);
-    edge1 = createSprite(400, 600, 800, 10)
-    edge1.shapeColor = "grey";
-    edge2 = createSprite(400, 0, 800, 10)
-    edge2.shapeColor = "grey";
-    edge3 = createSprite(800, 300, 10, 600)
-    edge3.shapeColor = "grey";
-    edge4 = createSprite(0, 300, 10, 600)
-    edge4.shapeColor = "grey";
-
-
+    var canvas = createCanvas(600,600);
     o1 = createSprite(210, 580, 100, 20)
     o1.shapeColor = "green";
     o2 = createSprite(100, 580, 100, 20)
@@ -30,8 +20,8 @@ function setup(){
     ball = createSprite(430, 500, 30, 30)
     ball.shapeColor = "white";
     var rand = Math.round(random(1,6));
-    ball.velocityY=2
-    ball.velocityX=2
+    ball.velocityY=5
+    ball.velocityX=3
     
  
     //create 4 different surfaces
@@ -44,19 +34,30 @@ function setup(){
 
 function draw() {
     background(rgb(169,169,169));
+    edges=createEdgeSprites();
+    if (isTouching(ball, o4)) {
+        ball.shapeColor = "yellow"
+        
+    }
+    if (isTouching(ball, o3)) {
+        ball.shapeColor = "red"
+    }
+    if (isTouching(ball, o2)) {
+        ball.shapeColor = "blue"
+        music.stop();
+        ball.velocityY=0
+        ball.velocityX=0
+    }
     if (isTouching(ball, o1)) {
         ball.shapeColor = "green"
+        music.play();
     }
     
-    bounceOff(edge1, ball);
-    bounceOff(edge2, ball);
-    bounceOff(edge3, ball);
-    bounceOff(edge4, ball);
-    bounceOff(o1, ball);
-    bounceOff(o2, ball);
-    bounceOff(o3, ball);
-    bounceOff(o4, ball);
+    
+    ball.bounceOff(edges)
+    
     //create edgeSprite
-        drawSprites();
+    drawSprites();
     //add condition to check if box touching surface and make it box
 }
+
